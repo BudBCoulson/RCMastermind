ed015ab2c1a7f6cf5f392cffbd32320e91316538
BudBCoulson <50926350+BudBCoulson@users.noreply.github.com>
2020-12-15 17:21:51 -0500

    Update placer_bot.py

--- placer_bot.py
+++ placer_bot.py
@@ -1,6 +1,5 @@
 from typing import Any, Optional
 
-
 class PlacerBot:
     BOTNAME = "Mastermind Placer"
     BOTEMOJI = "🧞"
@@ -9,6 +8,7 @@ class PlacerBot:
 
     def __init__(self, res_json):
         self.id = int(res_json["id"])
+        self.pegs = set()
 
     @classmethod
     def get_create_req(cls, world):
@@ -21,3 +21,27 @@ class PlacerBot:
             "emoji": PlacerBot.BOTEMOJI,
             "can_be_mentioned": False
         }}
+        
+    def place_wall(self, x, y, clr, txt = None):
+		jsn = {"bot_id": self.id
+		       "wall": {
+               "pos": {
+                 "x": x,
+                 "y": y
+               },
+               "color": clr,
+               "wall_text": txt
+            }}
+        res = post("",jsn)
+        self.pegs.add(res["id"])
+		
+	def erase_wall(self, idx):
+		if idx in self.pegs:
+			jsn = {"bot_id": self.id}
+			delete(idx,jsn)
+			self.pegs.remove(idx)
+		
+	def clear_board(self):
+		for idx in self.pegs:
+			self.erase_wall(idx)
+		self.pegs = set()
