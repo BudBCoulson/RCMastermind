87c406ea2886ff9cbe937ae69953b8cf8f223a9c
BudBCoulson <50926350+BudBCoulson@users.noreply.github.com>
2020-12-15 14:53:06 -0500

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -11,31 +11,44 @@ def numberToBase(n, b):
     return ''.join(digits[::-1])
 
 max_bound = int('5555',6)
-num = random.randrange(max_bound)
-numstr = numberToBase(num,6).zfill(4)
-numcnt = Counter(numstr)
-print(num,numstr,numcnt)
-  
-nstr = input("Guess the passcode:").zfill(4)
-n = int(nstr,6)
-ncnt = Counter(nstr)
-   
-if (n == num):   
-    print("Great! You guessed the number in just one try! You're a Mastermind!") 
-else: 
-    ctr = 0  
 
-    while (n != num):   
-        ctr += 1
-        
-        pos_correct = sum(nstr[-i-1] == numstr[-i-1] for i in range(min(len(numstr),4)))
-        offpos_correct = sum(min(ncnt[s],numcnt[s]) for s in numcnt if s in ncnt) - pos_correct
+def start_game():
+	num = random.randrange(max_bound)
+	numstr = numberToBase(num,6).zfill(4)
+	numcnt = Counter(numstr)
+	  
+	n, nstr, ncnt = get_guess()
+	   
+	if (n == num):   
+		print("Great! You guessed the number in just one try! You're a Mastermind!") 
+		
+	else: 
+		ctr = 0  
 
-        print("Black pegs", pos_correct)
-        print("White pegs", offpos_correct)
-        nstr = input("Enter your next choice of numbers: ").zfill(4)
-        n = int(nstr,6)
-        ncnt= Counter(nstr)
+		while (n != num):   
+			ctr += 1
+			
+			pos_correct, offpos_correct = feedback(nstr,ncnt,numstr,numcnt)
 
-    print("You've become a Mastermind!") 
-    print("It took you only", ctr, "tries.") 
+			print("Black pegs", pos_correct)
+			print("White pegs", offpos_correct)
+			
+			n, nstr, ncnt = get_guess()
+
+		print("You've become a Mastermind!") 
+		print("It took you only", ctr, "tries.") 
+
+def get_guess():
+	nstr = input("Guess the passcode:").zfill(4)
+	n = int(nstr,6)
+	ncnt = Counter(nstr)
+	
+	return n, nstr, ncnt
+	
+def feedback(nstr,ncnt,numstr,numcnt):
+	pos_correct = sum(nstr[-i-1] == numstr[-i-1] for i in range(min(len(numstr),4)))
+	offpos_correct = sum(min(ncnt[s],numcnt[s]) for s in numcnt if s in ncnt) - pos_correct
+	
+	return pos_correct, offpos_correct
+	
+start_game()
