9ec0568c3199a96051ea32ca080dc7658f3e34f9
BudBCoulson <50926350+BudBCoulson@users.noreply.github.com>
2020-12-16 10:39:59 -0500

    Update placer_bot.py

--- placer_bot.py
+++ placer_bot.py
@@ -5,15 +5,17 @@ from rc_rest_api import *
 class PlacerBot:
     BOTNAME = "Mastermind Placer"
     BOTEMOJI = "🧞"
-    START_X = 5
-    START_Y = 108
+    #START_X = 5
+    #START_Y = 108
     COLOR_NAMES = {"R": "pink", "O": "orange", "G": "green", "B": "blue", "P": "purple", "Y": "yellow"}
 
-    def __init__(self):
+    def __init__(self, start_x, start_y):
+        self.start_x = start_x
+        self.start_y = start_y
         req = {"bot": {
             "name": self.BOTNAME,
-            "x": self.START_X,
-            "y": self.START_Y,
+            "x": self.start_x,
+            "y": self.start_y,
             "emoji": self.BOTEMOJI,
             "direction": "left",
             "can_be_mentioned": False
@@ -61,27 +63,27 @@ class PlacerBot:
         
         print("PlacerBot: changed orientation to", direct)
         
-    def write_code(self, colors, turn):
+    def write_code(self, colors):
         for i in range(4):
-            self._place_wall(self.START_X-4+i, self.START_Y+turn, clr = self.COLOR_NAMES[colors[i]])
+            self._place_wall(self.start_x-4+i, self.start_y, clr = self.COLOR_NAMES[colors[i]])
         
-        print("PlacerBot: wrote code", colors, "at turn #", turn)
+        print("PlacerBot: wrote code", colors)
             
-    def write_keys(self, pos_c, off_c, turn):
-        self._place_wall(self.START_X+1, self.START_Y+turn, clr = 'pink', txt = str(pos_c))
-        self._place_wall(self.START_X+2, self.START_Y+turn, clr = 'gray', txt = str(off_c))
-        self._orient('up')
-        self._move_to(self.START_X+2, self.START_Y+turn+1)
-        self._orient('left')
+    def write_keys(self, pos_c, off_c):
+        self._place_wall(self.start_x-2, self.start_y, clr = 'pink', txt = str(pos_c))
+        self._place_wall(self.start_x-1, self.start_y, clr = 'gray', txt = str(off_c))
+        #self._orient('up')
+        #self._move_to(self.START_X+2, self.START_Y+turn+1)
+        #self._orient('left')
         
-        print("PlacerBot: wrote keys", pos_c, off_c, "at turn #", turn) 
+        print("PlacerBot: wrote keys", pos_c, off_c) 
                  
-    def clear_board(self):
+    def clear(self):
         while self.pegs:
             x, y, idx = self.pegs.pop()
             self._erase_wall(x,y,idx)
             
-        print("PlacerBot: board reset")
+        print("PlacerBot: walls cleared")
             
     def vanish(self):
         delete(self.id)
