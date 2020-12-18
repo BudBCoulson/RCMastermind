02355871bfa1362dcad88698ad23cdf071ff3ca4
BudBCoulson <50926350+BudBCoulson@users.noreply.github.com>
2020-12-17 23:09:15 -0500

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,5 @@
 import random
-from collections import Counter
+from collections import Counter, defaultdict
 
 def numberToBase(n, b):
     if n == 0:
@@ -10,33 +10,88 @@ def numberToBase(n, b):
         n //= b
     return "".join(digits[::-1])
 
+NUM_COLORS = 6
+CODE_LEN = 4   
+MAX_BOUND = NUM_COLORS**CODE_LEN - 1
+COLOR_CODES = {"R": "0", "O": "1", "G": "2", "B": "3", "P": "4", "Y": "5"}
+CODE_COLORS = {val: key for key, val in COLOR_CODES.items()}
+
 class Game:
-    MAX_BOUND = int("5555",6)
-    COLOR_CODES = {"R": "0", "O": "1", "G": "2", "B": "3", "P": "4", "Y": "5"}
-    CODE_COLORS = {val: key for key, val in COLOR_CODES.items()}
     
     def __init__(self):
-        self.secrets = self._generate_code()
+        self.code = self._generate_code()
         
-        print(f"Game: init with code data {self.secrets}")
-    
+        self.possible = set(range(MAX_BOUND))
+        self.combos = set(range(MAX_BOUND))
+        self.guess = 7
+        
+        print(f"Game: init with color code {self._codeforms(self.code)[-1]}")
+
     def _generate_code(self):
-        num = random.randrange(self.MAX_BOUND)
-        numstr = numberToBase(num,6).zfill(4)
+        num = random.randrange(MAX_BOUND)
+        return num
+
+    def _codeforms(self, num):
+        numstr = numberToBase(num,NUM_COLORS).zfill(CODE_LEN)
         numcnt = Counter(numstr)
-        clrstr = "".join(self.CODE_COLORS[d] for d in numstr)
-        
-        print("Game: generated secrets")
+        clrstr = "".join(CODE_COLORS[d] for d in numstr)
         
         return numstr, numcnt, clrstr
-        
-    def process_guess(self, guess):
-        nstr = "".join(self.COLOR_CODES.get(clr,"") for clr in guess).zfill(4)
+
+    def _clr_to_num(self, clrcode):
+        return int("".join(COLOR_CODES.get(clr,"") for clr in clrcode),NUM_COLORS)
+
+    def process_guess(self, guess=self.guess, code=self.code, human=True):
+        if type(guess) is str: # guess is a color code from an RC Avatar
+            nstr = "".join(COLOR_CODES.get(clr,"") for clr in guess).zfill(CODE_LEN)
+            self.guess = int(nstr, NUM_COLORS)
+        else: # guess is an int, internally stored "guess"
+            nstr = numberToBase(guess, NUM_COLORS).zfill(CODE_LEN)
         ncnt = Counter(nstr)
-        numstr, numcnt, _ = self.secrets
-        pos_correct = sum(nstr[-i-1] == numstr[-i-1] for i in range(min(len(numstr),4)))
+        numstr, numcnt, _ = self._codeforms(self.code)
+        
+        pos_correct = sum(nstr[-i-1] == numstr[-i-1] for i in range(min(len(numstr),CODE_LEN)))
         offpos_correct = sum(min(ncnt[s],numcnt[s]) for s in numcnt if s in ncnt) - pos_correct
         
-        print(f"Game: processed guess {guess} with results {pos_correct}, {offpos_correct}")
+        if human:
+            self._update_guesser(pos_correct, offpos_correct)
+            print(f"Game: processed guess {guess} with results {pos_correct}, {offpos_correct}")
         
         return pos_correct, offpos_correct
+
+    def get_guess_code(self):
+        return self._codeforms(self.guess)[-1]
+
+    def _update_guesser(self, pos_c, off_c):
+        self.possible.discard(self.guess)
+        self.combos.discard(self.guess)
+        
+        self._prune(pos_c, off_c)
+        self._minimax()
+
+    def _prune(self, pos_c, off_c):
+        newposs = set()
+        for cd in self.possible:
+            if (pos_c, off_c) == self.process_guess(code=cd, human=False):
+                newposs.add(cd)
+        self.possible = newposs
+
+    def _minimax(self):
+        scores = {}
+        
+        for gs in self.combos:
+			score_counts = defaultdict(int)
+            for cd in self.possible:
+                keypegs = self.process_guess(guess=gs, code=cd, human=False)
+                score_counts[keypegs] += 1
+            scores[gs] = max(score_counts.values())
+        
+        minmax = min(scores.values())
+        next_guesses = {gs for gs in scores if scores[gs] == minmax}
+        
+        for ng in next_guesses:
+            if ng in self.possible:
+                self.guess = ng
+                return
+                
+        self.guess = next_guesses.pop()
